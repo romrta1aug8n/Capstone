fcfc68a7479f3f3be90f40865d0eff58a93078ee
romrta1aug8n <romartalaugon123@gmail.com>
2024-12-12 01:17:29 +0800

    On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   admin.html 	modified:   css/admin.css 	deleted:    css/dashboard.css 	modified:   css/login_page.css 	deleted:    css/nav.css 	deleted:    dashboard.html 	new file:   js/button.js 	modified:   login_page.html

---
+++ js/button.js
@@ -0,0 +1,8 @@
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-content');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
