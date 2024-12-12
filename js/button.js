c8f350013da4dac8753da5521ad7011c1ecbfd5f
romrta1aug8n <romartalaugon123@gmail.com>
2024-12-12 19:48:25 +0800

    additional update  On branch main  Your branch is up to date with 'origin/main'.  Changes to be committed: 	modified:   admin.html 	modified:   css/admin.css 	modified:   js/button.js

--- js/button.js
+++ js/button.js
@@ -6,21 +6,3 @@ function toggleDropdown() {
         dropdownContent.style.display = 'block';
     }
 }
-
-function toggleDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-report');
-    if (dropdownContent.style.display === 'block') {
-        dropdownContent.style.display = 'none';
-    } else {
-        dropdownContent.style.display = 'block';
-    }
-}
-
-function toggleDropdown() {
-    const dropdownContent = document.querySelector('.dropdown-corn');
-    if (dropdownContent.style.display === 'block') {
-        dropdownContent.style.display = 'none';
-    } else {
-        dropdownContent.style.display = 'block';
-    }
-}
