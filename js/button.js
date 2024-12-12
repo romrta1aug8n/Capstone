e09f807ce7be6d56a2301a8f3f0275e20ac46214
romrta1aug8n <romartalaugon123@gmail.com>
2024-12-12 19:24:26 +0800

    update design On branch main Your branch is up to date with 'origin/main'. Changes to be committed: 	modified:   admin.html 	new file:   agri_engin_sector.html 	new file:   corn_sector.html 	modified:   css/admin.css 	new file:   css/corn_sector.css 	modified:   css/login_page.css 	modified:   css/register_page.css 	new file:   css/rice_sector.css 	new file:   fish_sector.html 	new file:   high_value_crop.html 	modified:   js/button.js 	new file:   livestock_sector.html 	modified:   login_page.html 	new file:   rice_sector.html

--- js/button.js
+++ js/button.js
@@ -6,3 +6,21 @@ function toggleDropdown() {
         dropdownContent.style.display = 'block';
     }
 }
+
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-report');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
+
+function toggleDropdown() {
+    const dropdownContent = document.querySelector('.dropdown-corn');
+    if (dropdownContent.style.display === 'block') {
+        dropdownContent.style.display = 'none';
+    } else {
+        dropdownContent.style.display = 'block';
+    }
+}
